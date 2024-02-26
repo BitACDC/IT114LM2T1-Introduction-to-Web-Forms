5b79e558c52d97192479010b4b6c2a96085512c2
BitACDC <154488752+BitACDC@users.noreply.github.com>
2024-02-27 07:14:19 +0800

    Update Exercise3.aspx.cs

--- Module2Topic1/Exercise3.aspx.cs
+++ Module2Topic1/Exercise3.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +11,68 @@ namespace Module1Exercise1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            // TODO 3.3 Set the text value of the finalGrade label to "Submit your grade percentage to see your final grade!". Watch out for post backs
+            if (!IsPostBack)
+            {
+                finalGrade.Text = "Submit your grade";
+            }
+        }
+
+        protected void Calculate_Click(object sender, EventArgs e)
+        {
+            double percentage;
+            if (double.TryParse(percentageGrade.Text, out percentage))
+            {
+                string grade;
+                if (percentage >= 96)
+                {
+                    grade = "1.00";
+                }
+                else if (percentage >= 91.51 && percentage <= 95.99)
+                {
+                    grade = "1.25";
+                }
+                else if (percentage >= 87.01 && percentage <= 91.50)
+                {
+                    grade = "1.50";
+                }
+                else if (percentage >= 82.51 && percentage <= 87.00)
+                {
+                    grade = "1.75";
+                }
+                else if (percentage >= 78.01 && percentage <= 82.50)
+                {
+                    grade = "2.00";
+                }
+                else if (percentage >= 73.51 && percentage <= 78.00)
+                {
+                    grade = "2.25";
+                }
+                else if (percentage >= 69.01 && percentage <= 73.50)
+                {
+                    grade = "2.50";
+                }
+                else if (percentage >= 64.51 && percentage <= 69.00)
+                {
+                    grade = "2.75";
+                }
+                else if (percentage >= 60.00 && percentage <= 64.50)
+                {
+                    grade = "3.00";
+                }
+                else if (percentage >= 63.00 && percentage <= 60.00)
+                {
+                    grade = "5";
+                }
+                else
+                {
+                    grade = "Not available";
+                }
+
+                finalGrade.Text = grade;
+
+               
+            }
+           
         }
     }
 }
